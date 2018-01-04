dbbf4d1151749de538efe0e39e7c8a87691020f5
Patrick Huber <patrick.huber1@gmail.com>
2018-01-03 18:13:09 -0600

    Adding new camera process mostly working with some static values. communication over BLE working well at this point. Need better way of getting available wifi networks from server

--- src/pages/camera-add/camera-add.ts
+++ src/pages/camera-add/camera-add.ts
@@ -11,15 +11,17 @@ import { MainPage } from '../pages';
   selector: 'page-camera-add',
   templateUrl: 'camera-add.html'
 })
+
 export class CameraAddPage {
   devices: any[] = [];
   statusMessage: string;
-  message: string;
-  currentStep: string = 'wifi';
+  currentStep: string = 'scan';
 
-  ovenUUID: string[] = ['a018']; // This will only search for the unique oven cam UUID
+  ovenCamUUID: string[] = ['a018']; // This will only search for the unique oven cam UUID
+  serviceUUID: string = 'a018';
 
-  wifi: { name: string, password: string } = {
+  wifi: { networks: string[], name: string, password: string } = {
+    networks: [],
     name: '',
     password: ''
   };
@@ -38,106 +40,106 @@ export class CameraAddPage {
     }
     return array.buffer;
   }
+  bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  }
 
   ionViewDidEnter() {
     console.log('ionViewDidEnter');
-    // this.scan();
+    this.scan();
   }
 
   scan() {
-    this.setStatus('Scanning for Bluetooth LE Devices');
+    // this.setStatus('Scanning for Bluetooth LE Devices');
     
     this.devices = [];  // clear list
-    
 
-    // this.message = 'Searching';
-
-    this.ble.scan(this.ovenUUID, 5).subscribe(
-      device => this.onDeviceDiscovered(device), 
+    this.ble.scan(this.ovenCamUUID, 5).subscribe(
+      device => {
+        if(device.name === 'oven-cam') {
+          this.onDeviceDiscovered(device)
+        } else {
+          this.scanError('Found device, but wrong name');
+        }
+      },
       error => this.scanError(error)
     );
 
     // setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
   }
 
+  // If location permission is denied, you'll end up here
+  scanError(error) {
+    this.setStatus('Error ' + error);
+  }
+
   onDeviceDiscovered(device) {
     console.log('Discovered ' + JSON.stringify(device, null, 2));
 
-    this.devices = [device];
+    // Todo: need to add check if there are multiple oven-cams detected
+    // Priority low (low probability of two cameras in setup mode at once)
     
-    if(device.name === 'oven-cam') {
-        this.setStatus('Camera found!');
+    // this.setStatus('Camera found!');
+    this.devices = [device];
 
-        this.connect(device);
-
-     
-    }
+    this.ngZone.run(function(){});
   }
 
-  connect(device) {
-    this.setStatus('Connecting to oven-cam...');
+  connect() {
+    var device: any = this.devices[0];
+
+    // this.setStatus('Connecting to oven-cam...');
 
     this.ble.connect(device.id).subscribe(
-      device => this.setStatus('connected!'), 
+      device => {
+        this.currentStep = 'wifi';
+        this.ngZone.run(function(){});
+      },
       error => this.setStatus(error)
     );
   }
 
-  testSend() {
-    // this.setStatus('connected! trying to write');
-    // Test to try and send data 
-        let name: ArrayBuffer = this.stringToBytes(this.wifi.name);
-        let pass: ArrayBuffer = this.stringToBytes(this.wifi.password);
-
-        // this.setStatus(pass);
-
-        this.ble.write(this.devices[0].id, 'a018', 'ec01', name).then(
-          () => this.setStatus('Name sent'),
-          e => this.setStatus('Error')
-        );
-        this.ble.write(this.devices[0].id, 'a018', 'ec00', pass).then(
-          () => this.setStatus('Password sent'),
-          e => this.setStatus('Error')
-        );
-
-        //this.ble.disconnect(this.devices[0].id);
+  // Todo: connect and get wifi networks from camera
+  // This is not currently used, but saving for future reference
+  readBLE() {
+    this.ble.read(this.devices[0].id, 'a018', 'ec00')
+      .then(
+        data => {
+          data = this.bytesToString(data);
+          this.setStatus(data);
+          // this.wifi.networks = data.split(',');
+          // this.currentStep = 'wifi';
+          // this.ngZone.run(function(){});
+        },
+        e => this.setStatus('Error getting available networks.')
+      );
   }
 
-  read() {
-    // Test to try and send data 
-        // let name: ArrayBuffer = this.stringToBytes(this.wifi.name);
-        // let pass: ArrayBuffer = this.stringToBytes(this.wifi.password);
+  submitWifi() {
+    var deviceId: string = this.devices[0].id;
+    var service: string = 'a018';
+    var charWifi: string = 'ec01';
 
-        // this.setStatus(pass);
+    let wifiCredentials: ArrayBuffer = this.stringToBytes(this.wifi.name + ',' + this.wifi.password);
 
-        this.ble.read(this.devices[0].id, 'a018', 'ec01').then(
-          (name) => this.setStatus('Name read: ' + String.fromCharCode.apply(null, new Uint8Array(name))),
-          e => this.setStatus('Error')
-        );
-        this.ble.read(this.devices[0].id, 'a018', 'ec00').then(
-          (pass) => this.setStatus('Password read: ' + String.fromCharCode.apply(null, new Uint8Array(pass))),
-          e => this.setStatus('Error')
-        );
+    this.writeBLE(deviceId, service, charWifi, wifiCredentials);
+  }
 
-        //this.ble.disconnect(this.devices[0].id);
+  writeBLE(device, service, characteristic, value): any {
+    this.ble.write(device, service, characteristic, value).then(
+      () => {
+        this.disconnect();
+        this.currentStep = 'success';
+        this.ngZone.run(function(){});
+      },
+      e => this.setStatus('Error connecting to wifi')
+    );
   }
 
   disconnect() {
     this.ble.disconnect(this.devices[0].id).then(
-      () => this.setStatus('Disconnected'),
       e => this.setStatus('Error disconnecting')
     );;
-  }
-
-  // If location permission is denied, you'll end up here
-  scanError(error) {
-    this.setStatus('Error ' + error);
-    // let toast = this.toastCtrl.create({
-    //   message: 'Error scanning for Bluetooth low energy devices',
-    //   position: 'middle',
-    //   duration: 5000
-    // });
-    // toast.present();
   }
 
   setStatus(message) {
@@ -145,22 +147,5 @@ export class CameraAddPage {
     // this.ngZone.run(() => {
     //   this.statusMessage = message;
     // });
-  }
-
-  stepWifi() {
-    this.currentStep = 'wifi';
-  }
-
-  sendWifiInfo(event) {
-    this.scan();
-
-    let pass: ArrayBuffer = this.stringToBytes(this.wifi.password);
-
-    // this.setStatus(pass);
-
-    // this.ble.write(this.devices[0].id, 'a018', 'ec00', pass).then(
-    //   () => this.setStatus('Password sent'),
-    //   e => this.setStatus('Error')
-    // );
   }
 }
