b1dcd7d1bc9ca69f092ac104d3c58100ee3fd911
Patrick Huber <patrick.huber1@gmail.com>
2018-03-20 09:57:55 -0400

    Cleanup timers provider; set default page to welcome

--- src/providers/timer/timer.ts
+++ src/providers/timer/timer.ts
@@ -4,10 +4,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 /*
-  Generated class for the TimerProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
+  Timer alert with local notifications
 */
 @Injectable()
 
@@ -25,26 +22,20 @@ export class Timers {
   timers = [];
   timerId = 0;
 
-
   constructor(public storage: Storage, public localNotifications: LocalNotifications, public platform: Platform, public events: Events) {}
 
   load() {
-    return this.storage.get(this.TIMERS_KEY).then((value) => {
-      if (value.length > 0) {
+    return this.storage.get(this.TIMERS_KEY).then((storedTimers) => {
+      if (storedTimers.length > 0) {
         this.timers = [];
-        for (let timer of value) {
-          console.log(timer.timeRemaining)
+        for (let timer of storedTimers) {
           this.updateTimeRemaining(timer);
-          console.log(timer.timeRemaining)
           if(timer.timeRemaining > 0) {
             this.timers.push(timer);
-            console.log('setting notifications for stored timers');
             this.setNotification(timer);
           }
           // Todo: add check for past timers? Do we need to notifiy of missed timer?
         }
-        console.log('loaded stored timers');
-        console.log(this.timers);
       }
 
       this.events.publish('timers:loaded', this.timers);
@@ -62,28 +53,18 @@ export class Timers {
     };
     this.timerId++;
 
-    console.log('setting notifications for new timer');
     this.setNotification(newTimer);
-
-    // this.updateTimeRemaining(newTimer);
-
-    console.log('timer added');
 
     this.events.publish('timers:added', newTimer);
     
     this.timers.push(newTimer);
     return this.storage.set(this.TIMERS_KEY, this.timers);
-
   }
 
   removeTimer(id: number) {
-    console.log('remove timer');
-    console.log(this.timers);
     this.timers = this.timers.filter(function( obj ) {
       return obj.id !== id;
     });
-    console.log('made it here');
-    console.log(this.timers);
     this.storage.set(this.TIMERS_KEY, this.timers);
     return this.timers;
   }
@@ -115,37 +96,27 @@ export class Timers {
   }
 
   setNotification(timer: Timer) {
-     let notification = {
-        id: timer.id,
-        title: 'Oven Cam Timer',
-        text: 'Timer for ' + this.format(timer.timerLength) + ' is done.',
-        at: new Date(timer.startTime.getTime() + timer.timerLength)
+    let notification = {
+      id: timer.id,
+      title: 'Oven Cam Timer',
+      text: 'Timer for ' + this.format(timer.timerLength) + ' is done.',
+      at: new Date(timer.startTime.getTime() + timer.timerLength)
     };
 
-    // this.notifications.push(notification);
-
-
     if(this.platform.is('cordova')){
-
       // TODO: test if notifications carry over after reloading app and initilizing current timers from storage
       // this.localNotifications.cancelAll().then(() => {
-
         // Schedule the new notifications
         this.localNotifications.schedule(notification);
-        // this.notifications = [];
-
-        console.log('notification set');
       // }); 
     }
   }
 
   updateTimeRemaining(timer: Timer) {
-    console.log('updateTimeRemaining')
     var timeEllapsed = Math.abs(new Date().getTime() - timer.startTime.getTime());
     var timeRemaining = timer.timerLength - timeEllapsed;
     timer.timeRemaining = timeRemaining;
     return timer;
-    
   }
 
   get allTimers() {
