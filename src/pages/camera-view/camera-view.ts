22e2d1cfee4853e8ca1d8df93412d6e3db9517ce
Patrick Huber <patrick.huber1@gmail.com>
2018-05-03 11:27:48 -0500

    Fixed issue with alerts page initializing twice after navigating from camera stream. Using tabs parent select for navigation instead of navPush

--- src/pages/camera-view/camera-view.ts
+++ src/pages/camera-view/camera-view.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
 import { Timers } from '../../providers/providers';
 
@@ -18,54 +18,19 @@ import { Timers } from '../../providers/providers';
 export class CameraViewPage {
 
   _timers: any = this.timers.timers;
-  _tickTimers: any = new Array();
-  currentTime: any = new Date();
-  tickTimeout: any = new Array();
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    public timers: Timers,
-    public events: Events) {
-    events.subscribe('timers:loaded', allTimers => {
-      // console.log(this._timers);
-      this._timers = allTimers;
-      // this.initTimers();
-    });
-    events.subscribe('timers:added', newTimer => {
-      this._timers = timers.allTimers;
-      // this.tick(newTimer)
-    });
-    events.subscribe('timers:removed', removedTimerId => {
-      this._timers = timers.allTimers;
-    });
+    public timers: Timers) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraViewPage');
   }
 
-  initTimers() {
-    for (let timer of this._timers) {
-      this.tick(timer);
-    }
-
-    // add test timer
-    // this.timers.addTimer(5000);
-  }
-
-  tick(timer) {
-    this.tickTimeout[timer.id] = setTimeout(() => {
-      if (timer.timeRemaining < 1000) {
-        return this._timers = this.timers.removeTimer(timer.id);
-      }
-      timer.timeRemaining = timer.timeRemaining - 1000;
-      this.tick(timer);
-    }, 1000);
-  }
-
   navTimersPage(){
-    this.navCtrl.push('AlertsPage');
+    // Navigate to Alerts tab - can't use navPush since that re-inits the alerts page
+    this.navCtrl.parent.select(1);
   }
 
 }
